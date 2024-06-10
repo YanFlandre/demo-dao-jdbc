4925ead2802aa6db2859a86293996d9be4da5cdb
YanFlandre <jp_felippe@hotmail.com>
2024-06-10 09:58:05 -0300

    Adding seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import modelentities.Department;
+import modelentities.Seller;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args){
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, obj, 3000.0, new Date(), "bob@gmail.com", "Bob");
+        System.out.println(seller);
     }
 }
